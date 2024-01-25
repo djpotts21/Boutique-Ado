a75da5d42dfd37182fe9f83545dad9d5b6fa15b2
Daniel Potts. <djpotts2891@gmail.com>
2024-01-25 16:17:49 +0000

    Add toasts and functions, updated hassizes to has_sizes

--- products/models.py
+++ products/models.py
@@ -1,27 +1,31 @@
 from django.db import models
 
+
 class Category(models.Model):
+
     class Meta:
-        verbose_name_plural = 'Categories' # This will change the name of the model in the admin panel to "Categories" instead of "Categorys"
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    
+
     def __str__(self):
         return self.name
-        
+
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL) # Foreign key to the category model
-    sku = models.CharField(max_length=254, null=True, blank=True) # SKU is a unique identifier for each product
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    hassize = models.BooleanField(default=False, null=True, blank=True) # Boolean field to check if the product has sizes
-    price = models.DecimalField(max_digits=6, decimal_places=2) # Decimal field to store the price
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) # Decimal field to store the rating
-    image_url = models.URLField(max_length=1024, null=True, blank=True) # URL field to store the image URL
-    image = models.ImageField(null=True, blank=True) # Image field to store the image itself
-    
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
     def __str__(self):
         return self.name
