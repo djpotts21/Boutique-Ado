af8e181383446ac7231f2ddf205cc96938abec07
Daniel Potts. <djpotts2891@gmail.com>
2024-02-03 11:31:11 +0000

    Fix issue with callback URL from stripe

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from .webhooks import webhook
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
+    path('wh/', csrf_exempt(webhook), name='webhook'),
 ]
