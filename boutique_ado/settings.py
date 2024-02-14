d4c6e13d369aa80a6a389a7d7cf5e59a5cfbd54e
Daniel Potts. <djpotts2891@gmail.com>
2024-02-14 11:42:45 +0000

    Updated requirements and settings.py secretkey for heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-okbcijwcm-yyrp91sj)i+=em6ioq!np@s7l()gfp2wq&anp2_1'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
